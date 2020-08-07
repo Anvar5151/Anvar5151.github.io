9167326510e415aa685dec18468288d3f3c12f12
Anvar5151 <66218488+Anvar5151@users.noreply.github.com>
2020-08-07 17:11:10 +0500

    all
    good

---
+++ js/main.js
@@ -0,0 +1,96 @@
+ScrollReveal().reveal('.headline', {
+  delay: 400
+})
+ScrollReveal().reveal('.tagline', {
+  delay: 500
+})
+ScrollReveal().reveal('.punchline', {
+  delay: 1000
+})
+
+
+ScrollReveal().reveal('.widget', {
+  delay: 2000,
+  interval: 200
+});
+
+
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate_1-left', {
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 1000
+});
+
+sr.reveal('.animate_2-top', {
+  origin: 'top',
+  duration: 1000,
+  distance: '25rem',
+  delay: 300
+});
+
+sr.reveal('.animate_3-right', {
+  origin: 'right',
+  duration: 1000,
+  distance: '25rem',
+  delay: 400
+});
+
+sr.reveal('.animate_4-top', {
+  origin: 'top',
+  duration: 1000,
+  distance: '25rem',
+  delay: 900
+});
+
+sr.reveal('.animate_5-bottom', {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25rem',
+  delay: 900
+});
+
+sr.reveal('.animate_6-left', {
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 400
+});
+
+sr.reveal('.animate_7-bottom', {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25rem',
+  delay: 300
+});
+
+sr.reveal('.animate_8-right', {
+  origin: 'right',
+  duration: 1000,
+  distance: '25rem',
+  delay: 1000
+});
+
+sr.reveal('.animate-left', {
+  origin: 'left',
+  duration: 1000,
+  distance: '25rem',
+  delay: 1300
+});
+
+sr.reveal('.animate-right', {
+  origin: 'right',
+  duration: 1000,
+  distance: '25rem',
+  delay: 1300
+});
+
+sr.reveal('.animate-bottom', {
+  origin: 'bottom',
+  duration: 1000,
+  distance: '25rem',
+  delay: 1400
+});
